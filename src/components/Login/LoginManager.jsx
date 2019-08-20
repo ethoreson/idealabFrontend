af529726e3a14cb7708880114d30bc1333152f74
Patrick Poole <patrickcpoole@gmail.com>
2019-08-19 19:27:18 -0600

    functional component

--- src/components/Login/LoginManager.jsx
+++ src/components/Login/LoginManager.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-class LoginManager extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-          <p>Build Main Login Manager logic here!</p>
-      </div>
-    );
-  }
-}
+const LoginManager = () => {
+	return (
+		<div>
+			<p>This is a functional component</p>
+		</div>
+	);
+};
 
 export default LoginManager;
