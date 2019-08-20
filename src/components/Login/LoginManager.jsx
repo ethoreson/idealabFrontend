f53e6889a18221a90381b1fd39cff4dded167854
Patrick Poole <patrickcpoole@gmail.com>
2019-08-19 19:48:56 -0600

    test commit

--- src/components/Login/LoginManager.jsx
+++ src/components/Login/LoginManager.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
 class LoginManager extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-          <p>Build Main Login Manager logic here!</p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<p>This WILL be a functional component</p>
+			</div>
+		);
+	}
 }
 
 export default LoginManager;
